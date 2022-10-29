a1845ef41c1ee7e885f7fee75fee2498eb3a397c
Roni Andarsyah <roni.andarsyah@gmail.com>
2022-10-29 15:42:23 +0700

    Perubahan Appnya

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
-  return <div className="App">Selamat Data di Dashboard Geologi</div>;
+  return (
+    <div class="wrapper">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
