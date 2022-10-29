e1c4023db2bd5afe6ea99346ed45bc7583788c21
Roni Andarsyah <roni.andarsyah@gmail.com>
2022-10-29 21:17:57 +0700

    Edit Footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 
 export default class Footer extends Component {
   render() {
-    return <div>Ini Footer</div>;
+    return (
+      <div>
+        <footer class="main-footer">
+          <strong>
+            Copyright &copy; 2022<a href="https://www.roniandarsyah.com/">Geologi Bandung.</a>
+          </strong>
+          All rights reserved.
+          <div class="float-right d-none d-sm-inline-block">
+            <b>Version Ujicoba</b> 1.0
+          </div>
+        </footer>
+
+        <aside class="control-sidebar control-sidebar-dark"></aside>
+      </div>
+    );
   }
 }
