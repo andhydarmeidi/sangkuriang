53f9ff995ec7cd6026f8f958c070b25a581652b8
Roni Andarsyah <roni.andarsyah@gmail.com>
2022-10-29 15:49:43 +0700

    ganti

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Menu from './Menu';
+import Dashboard from './Dashboard';
+import Footer from './Footer';
 
 function App() {
   return (
     <div class="wrapper">
       <Header />
+      <Menu />
+      <Dashboard />
+      <Footer />
     </div>
   );
 }
